5ad29b274ededd1487cb812caca87e1014631526
hrothgar <hrothgar@risky.services>
2025-12-13 05:57:36 -0500

    Added stub for HGAnimationCurveMask and basic support for parsing AnimatorController

---
+++ AnimeStudio/Classes/HGAnimationCurveMask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimeStudio
+{
+    public sealed class HGAnimationCurveMask : NamedObject
+    {
+       public HGAnimationCurveMask(ObjectReader reader) : base(reader)
+        {
+            // TODO
+        }
+    }
+}
